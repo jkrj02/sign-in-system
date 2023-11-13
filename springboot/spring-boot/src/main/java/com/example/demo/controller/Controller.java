4d73648e102883ed9c45e29ce1bde9c060d5658c
xiaohua616 <114397536+xiaohua616@users.noreply.github.com>
2023-11-13 12:37:54 +0800

    Update Controller.java

--- springboot/spring-boot/src/main/java/com/example/demo/controller/Controller.java
+++ springboot/spring-boot/src/main/java/com/example/demo/controller/Controller.java
@@ -145,7 +145,141 @@ public class Controller {
         return studentService.findById(id);
     }
 
+    @Transactional
+    @PostMapping ("signin")
+    public boolean siggin(@RequestParam int userId, int classroomId, String signintime)
+    {   System.out.println("Hello, World!");
+        //String returninfo = "noactivity_in_this_room_at_this_time";
 
+        String sql = "SELECT * FROM activity where classroom_id =";
+        sql += String.valueOf(classroomId);
+
+        Query query = entityManager.createNativeQuery(sql, Activity.class);//指定返回类型
+        List<Activity> temp= query.getResultList();
+
+        String[] results = signintime.split("/|:");
+        int acttime = 0;
+        for(String sig:results)
+        {
+            acttime = acttime*100+Integer.parseInt(sig);
+        }
+        int activityid=-1;
+        if(temp.isEmpty()) return false;
+          // return "noactivity_in_this_room_at_this_time";
+        else {
+            for (Activity ppp : temp) {
+
+                String starttime =ppp.getTime() ;
+                String[] results1 = signintime.split("/|:");
+                int actstarttime = 0;
+                for(String sig:results1)
+                {
+                    actstarttime = actstarttime*100+Integer.parseInt(sig);
+                }
+                String endtime = ppp.getEndTime();
+                String[] results2 = signintime.split("/|:");
+                int actendtime = 0;
+                for(String sig:results2)
+                {
+                    actendtime = actendtime*100+Integer.parseInt(sig);
+                }
+                if(actstarttime<=acttime && acttime<=actendtime )
+                {
+                    activityid=ppp.getActivityId();
+                }
+            }
+        }
+        if(activityid ==-1)    return false;
+            //return  "noactivity_in_this_room_at_this_time";
+
+        String sql2 = "SELECT * FROM signin where activity_id = ";
+        sql2 += String.valueOf(activityid);
+        sql2 += " and user_id = ";
+        sql2 += String.valueOf(userId);
+
+        Query query2 = entityManager.createNativeQuery(sql2, Signin.class);//指定返回类型
+        List<Signin> temp2= query2.getResultList();
+        if(temp2.isEmpty()) return false;
+            //return "noactivity_in_this_room_at_this_time";
+        else{
+            String sql3= "UPDATE signin SET signin = '已签到' where activity_id = ";
+            sql3 += String.valueOf(activityid);
+            sql3 += " and user_id = ";
+            sql3 += String.valueOf(userId);
+            entityManager.createNativeQuery(sql3).executeUpdate();
+            return true;
+            //return "successfully_sign for user:"+Integer.toString(userId)+"for activity"+Integer.toString(activityid);
+        }
+    }
+
+    @PostMapping ("checkatt")
+    public Object checkatt(@RequestParam int teacherId, String start_time, String end_time)
+    {
+        System.out.println("Hello, World!");
+
+        String[] results0 = start_time.split("/|:");
+        int stime = 0;
+        for(String sig:results0)
+        {
+            stime = stime*100+Integer.parseInt(sig);
+        }
+        String[] results1 = end_time.split("/|:");
+        int etime = 0;
+        for(String sig:results1)
+        {
+            etime = etime*100+Integer.parseInt(sig);
+        }
+
+        String sql = "SELECT * FROM activity where organizer_id =";
+        sql += String.valueOf(teacherId);
+        sql +=" and type = '上课' and approved = '已通过'";
+        Query query = entityManager.createNativeQuery(sql, Activity.class);//指定返回类型
+        List<Activity> temp= query.getResultList();
+
+        class classandrate
+        {
+            public String couresename;
+            public double attendence;
+        }
+        List<classandrate> classandrates = new ArrayList<classandrate>();
+
+        for (Activity ppp : temp)
+        {
+            int tstime = 0 ;
+            int tetime = 0 ;
+            String[] results2 = ppp.getTime().split("/|:");
+            for(String sig:results2)
+            {
+                tstime = tstime*100+Integer.parseInt(sig);
+            }
+            String[] results3 = ppp.getEndTime().split("/|:");
+            for(String sig:results3)
+            {
+                tetime = tetime*100+Integer.parseInt(sig);
+            }
+            if(tstime >= stime && tetime <= etime)//
+            {
+                classandrate temp2 = new classandrate();
+                temp2.couresename = ppp.getName();
+                int total = 0;
+                int come = 0;
+                System.out.println("Hello, World!!!");
+                String sql1 = "SELECT * FROM signin where activity_id =";
+                sql1 += String.valueOf(ppp.getActivityId());
+                Query query1 = entityManager.createNativeQuery(sql1, Signin.class);//指定返回类型
+                List<Signin> temp3 = query1.getResultList();
+                for (Signin ptp : temp3) {
+                    total += 1;
+                    if (ptp.getSignin().equals("已签到")) come += 1;
+                }
+                temp2.attendence = (double)come / (double)total;
+                classandrates.add(temp2);
+                System.out.println(temp2.couresename+String.valueOf(temp2.attendence )+String.valueOf(come)+String.valueOf(total));
+            }
+        }
+        System.out.println(classandrates);
+        return classandrates;
+    }
 
 
 }
