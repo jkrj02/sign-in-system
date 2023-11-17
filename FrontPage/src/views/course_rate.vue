fcacc54e9d1b7c3de5b6a8d8b618cc28dac57431
dhsuBIT <100430401+dhsuBIT@users.noreply.github.com>
2023-11-17 11:28:42 +0800

    Add files via upload

--- FrontPage/src/views/course_rate.vue
+++ FrontPage/src/views/course_rate.vue
@@ -2,32 +2,49 @@
     <h2 class="head">
         课程出勤率查询
     </h2>
-    <h3>ID= {{ user_id }}</h3>
-
-    <el-input v-model="input1" style="width: 500px; margin: 20px 160px;"
-              placeholder="请输入查询起始日期（格式：年-月-日，如2023/11/11/9:00）"></el-input>
-    <el-input v-model="input2" style="width: 500px; margin: 20px 40px;"
-              placeholder="请输入查询截止日期（格式：年-月-日，如2023/11/11/13:00）"></el-input>
-
-    <router-link :to="{path:'/show_course_rate',query:{user_id:this.user_id}}">
-        <el-button type="success" class="button" style="width: 100px; margin: 20px 700px;">查询</el-button>
-    </router-link>
+    
+    <el-input v-model="input1" style="width: 500px; margin: 20px 160px;" placeholder="请输入查询起始日期（格式：年:月:日:小时:分钟，如2023:11:11:9:00）"></el-input>
+    <el-input v-model="input2" style="width: 500px; margin: 20px 40px;" placeholder="请输入查询截止日期（格式：年:月:日:小时:分钟，如2023:11:11:13:00）"></el-input>
+    <div v-if="response">
+      <h3>课程出勤率:</h3>
+      <pre>{{ response }}</pre>
+    </div>
+    <el-button @click="checkAttendance" type="success" class="button" style="width: 100px; margin: 20px 700px;">查询</el-button>
 </template>
-
 <script>
+import axios from 'axios'
+
 export default {
-    data() {
-        return {
-            input1: '',
-            input2: '',
-            user_id: '',
-        }
-    },
+  data() {
+    return {
+      input1: '',
+      input2: '',
+      response: null
+    }
+  },
 
+  methods: {
+    async checkAttendance() {
+      try {
+        var config = {
+          method: 'get',
+          headers: {
+              'Access-Control-Allow-Origin': 'http://localhost:5173'},
+          url: 'http://10.62.57.169:8080/checkatt?teacherId=100&start_time=2023:11:11:9:00&end_time=2023:11:11:13:00',
+        };
 
-    created() {
-        this.user_id = this.$route.query.user_id;
-    },
-}
+        const response = await axios(config);
+        this.response = response.data;
+        console.log(JSON.stringify(response.data));
+      }
+      catch (error) {
+        console.error('There was an error!', error);
+        this.response = 'Error: ' + error.message;
+      }
+    }
+  }
+};
+</script>
 
+<script setup>
 </script>
