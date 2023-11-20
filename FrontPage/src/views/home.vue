8633f1e07d2610321b40597e56aac04b4aa7300a
dhsuBIT <100430401+dhsuBIT@users.noreply.github.com>
2023-11-20 17:46:02 +0800

    Add files via upload

--- FrontPage/src/views/home.vue
+++ FrontPage/src/views/home.vue
@@ -1,108 +1,72 @@
 <template>
-    <!-- <h2 class="head">
-        我的日程 11月14号
-    </h2> -->
-    <h3>ID= {{ user_id }}</h3>
-    <router-link :to="{path:'/login_page'}">
-        <el-button type="success" class="button1">登录</el-button>
+  <h2 class="head">
+    Hello World!
+  </h2>
+  <h3 class="head">
+    ID = {{ user_id }}
+  </h3>
+  <div class="button-container">
+    <router-link :to="{path:'/login_page', query: {user_id: user_id}}">
+      <el-button type="success">登录</el-button>
     </router-link>
-
-    <!-- <el-card class="box-card">
-        <template #header>
-            <div class="card-header">
-                <span class="title">日程1</span>
-            </div>
-        </template>
-        <div class="text item">{{ '时间：8:00 ' }}</div>
-        <div class="text item">{{ '教室：中教101 ' }}</div>
-    </el-card>
-
-    <el-card class="box-card">
-    <template #header>
-      <div class="card-header">
-        <span class="title">日程2</span>
-      </div>
-    </template>
-    <div class="text item">{{ '时间：2:00 ' }}</div>
-    <div class="text item">{{ '教室：中教301 ' }}</div>
-  </el-card>
-
-    <el-card class="box-card">
-    <template #header>
-      <div class="card-header">
-        <span class="title">日程3</span>
-      </div>
-    </template>
-    <div class="text item">{{ '时间：18:00 ' }}</div>
-    <div class="text item">{{ '教室：3号楼301 ' }}</div>
-  </el-card> -->
-
-    <router-link :to="{path:'/create_activity',query:{user_id:this.user_id}}">
-        <el-button type="success" class="button1">活动申请</el-button>
+  </div>
+  <div class="button-container">
+    <router-link :to="{path:'/schedule', query: {user_id: user_id}}">
+      <el-button type="success">我的日程</el-button>
     </router-link>
-
-    <router-link :to="{path:'/rate',query:{user_id:this.user_id}}">
-        <el-button type="success" class="button1">出勤率查询</el-button>
+  </div>
+  <div class="button-container">
+    <router-link :to="{path:'/rate', query: {user_id: user_id}}">
+      <el-button type="success">出勤率</el-button>
     </router-link>
-
-    <router-link :to="{path:'/history',query:{user_id:this.user_id}}">
-        <el-button type="success" class="button1">历史日程</el-button>
-    </router-link>
-    <router-link :to="{path:'/schedule',query:{user_id:this.user_id}}">
-        <el-button type="success" class="button1">日程查询</el-button>
-    </router-link>
-
+  </div>
+  <div class="button-container">
+    <el-button @click="closeWindow" type="success">退出</el-button>
+    <!-- 加个关闭页面的功能 -->
+  </div>
 </template>
 
-<style> 
-/* .card-header {
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-} */
 
-/* .head {
+<style> 
+.head {
   margin-top: 40px;
   text-align-last: center;
-} */
+}
 
-/* .title {
-  font-size: 18px;
-} */
+.button-row {
+  display: flex;
+  justify-content: center; /* 使按钮在容器中居中 */
+  gap: 1px; /* 按钮之间的间隔 */
+}
 
-/* .text {
-  font-size: 18px;
-} */
+.button-container {
+  display: flex;
+  justify-content: center;
+  width: 100%;
+  margin-bottom: 20px;
+}
 
-/* .item {
-  margin-bottom: 14px;
-} */
-
-/* .box-card {
-  margin: 20px auto;
-  width: 480px;
-} */
-
-.button1 {
-  margin: 20px;
-  margin-left: 280px;
+.button{
+  margin: 20px 0; /* 垂直边距保留，水平边距移除 */
+  flex-grow: 1; /* 使按钮在水平方向上伸缩 */
+  max-width: 100px; /* 可以根据需要设置最大宽度 */
 }
 
 </style>
 
 <script>
 export default {
-    setup() {
+  setup() {
 
-    },
-    created() {
-        this.user_id = this.$route.query.user_id;
-    },
-    data() {
-        return {
-            user_id: '',
-        }
-    }
+  },
+  created() {
+      this.user_id = this.$route.query.user_id;
+  },
+  data() {
+      return {
+          user_id: '',
+      }
+  },
 }
 
 </script>
