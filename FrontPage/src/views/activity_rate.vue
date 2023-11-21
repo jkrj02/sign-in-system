ab4984bb4ec081f83868972cd14f9a127fe7cf7e
Evolte-ZGQ <115629870+Evolte-ZGQ@users.noreply.github.com>
2023-11-21 17:48:15 +0800

    Add files via upload

--- FrontPage/src/views/activity_rate.vue
+++ FrontPage/src/views/activity_rate.vue
@@ -1,10 +1,8 @@
 <template>
   <h2 class="head">
-      活动出勤率: 100%
+    活动出勤率: {{ this.response }} %
   </h2>
-  <h3 class="head">
-    ID = {{ user_id }}
-  </h3>
+
   <div class="button-container">
     <el-button @click="returnRate" type="success">返回</el-button>
   </div>
@@ -32,11 +30,38 @@
 </style>
 
 <script>
+import axios from 'axios';
+
 export default {
-methods: {
-returnRate(){
-  this.$router.go(-1)// 返回上一步
-},
-}
+  data() {
+    return {
+      response: null
+    };
+  },
+  methods: {
+    async checkAttendance() {
+      try {
+        var config = {
+          method: 'get',
+          headers: {'Access-Control-Allow-Origin': 'http://localhost:5173'},
+          url: 'http://10.63.110.16:8080/myattendence/1120209999',
+        };
+
+        const response = await axios(config);
+        this.response = (response.data* 100).toFixed(2);
+        console.log(JSON.stringify(response.data));
+      }
+      catch (error) {
+        console.error('There was an error!', error);
+        this.response = 'Error: ' + error.message;
+      }
+    },
+    returnRate() {
+      this.$router.go(-1); // 返回上一步
+    },
+  },
+  mounted() {
+    this.checkAttendance(); // 组件挂载后立即调用
+  }
 }
 </script>
